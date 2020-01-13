2622aed2ced3ff58a0b9132e663d5221f096b9bf
bp96 <brijeshpatel96@gmail.com>
2020-01-13 21:23:37 +0000

    uploading local project files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Navbar from './components/Navbar.js'
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      scrolled: false, 
+    }
+  }
+  
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='content'>
+
+      <section className='section-1 parallax'>
+        <Navbar />
+      </section>
+      
+        <section className='section-2'> 
+        <p> Due to the limited amount of tables in our restaurant, we cannot guarantee a private table. Some reservations will be sat at the bar or communal. If you prefer a table, please call or make a note and we will do our best to accommodate your request. </p>
+        </section>
+
+        <section className='section-3' >
+          
+          <p className='description'>
+            <h3> LA MARCHA TAPAS BAR </h3> 
+            With its rustic interior, pops of bright color and welcoming atmosphere, La Marcha is a tapas bar in the heart of Berkeley that encapsulates the vibrant culinary culture of Spain. Visitors will feel as though they have strolled right off the streets of Spain and into an authentic neighborhood tapas place. 
+          </p>
+          <img className='image' src='/images/restimage2.jpg' />
+        
+        </section>
+
+        <section className='section-4 parallax'> </section>
+
+        <section className='section-5'> </section>
+
+
+
     </div>
   );
 }
+}
 
 export default App;
+
+// creating: https://www.lamarchaberkeley.com/
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import './NavbarBootstrap.css';
+
+class NavBar extends Component {
+render() {
+  return (
+<Navbar className='generalNavbar' fixed='top' expand="lg">
+  <Navbar.Brand href="">Logo</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className='ml-auto nav-item'>
+      <Nav.Link href="" className='nav-item'>Home</Nav.Link>
+      <NavDropdown className='nav-item' title="Our Menu" id="basic-nav-dropdown">
+        <NavDropdown.Item className='nav-item' href="">Dinner</NavDropdown.Item>
+        <NavDropdown.Item className='nav-item' href="">Dessert</NavDropdown.Item>
+        <NavDropdown.Item className='nav-item' href="">Wine</NavDropdown.Item>
+        <NavDropdown.Item className='nav-item' href="">Sherries</NavDropdown.Item>
+        <NavDropdown.Item className='nav-item' href="">Paella</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link className='nav-item' href="">Happy Hour</Nav.Link>
+      <Nav.Link className='nav-item' href="">Events</Nav.Link>
+      <Nav.Link className='nav-item' href="">Catering</Nav.Link>
+      <Nav.Link className='nav-item' href="">Gallery</Nav.Link>
+      <Nav.Link className='nav-item' href="">Press</Nav.Link>
+      <Nav.Link className='nav-item' href="">Reserve Now</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+)
+}
+}
+
+export default NavBar;
